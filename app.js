d419dce58f76f35536aa8c40358974ba9ec92924
Julien879465 <beillonnet@laposte.net>
2023-02-23 10:56:29 +0100

    08

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 require("dotenv").config();
+const {
+  hashPassword,
+  verifyPassword,
+  verifyToken,
+  verifyTokenId,
+} = require("./auth");
 
 const express = require("express");
 
@@ -11,41 +17,29 @@ const port = process.env.APP_PORT ?? 5000;
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
-const { hashPassword, verifyPassword } = require("./auth");
-app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
 const userHandlers = require("./userHandlers");
 
+app.get("/", welcome);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
-
-const isItDwight = (req, res) => {
-  if (
-    req.body.email === "dwight@theoffice.com" &&
-    req.body.password === "123456"
-  ) {
-    res.send("Credentials are valid");
-  } else {
-    res.sendStatus(401);
-  }
-};
-
 app.post(
   "/api/login",
   userHandlers.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
+
+app.use(verifyToken);
+
+app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.put("/api/users/:id", hashPassword, verifyTokenId, userHandlers.updateUser);
+app.delete("/api/users/:id", verifyTokenId, userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
